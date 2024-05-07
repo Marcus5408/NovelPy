fe3662b79a1dcfe9c40668820583e7cc78823805
Issac Liu <issac@matchaflavored.site>
2024-05-07 09:31:51 -0400

    feat: Add example usage of pytale

---
+++ game/__init__.py
@@ -0,0 +1,13 @@
+import pytale as pt
+
+class Example:
+    def __init__(self):
+        self.name = "Example"
+        self.description = "This is an example game"
+        self.version = "0.0.1"
+
+    def run(self):
+        pt.run(self.name, self.description, self.version)
+
+if __name__ == "__main__":
+    Example().run()

6bc9babc49434a61d4818a8a58f6de1f91586837
Issac Liu <issac@matchaflavored.site>
2024-06-11 08:57:31 -0400

    refactor: Refactor character class to use more descriptive variable names

--- visnovel/__init__.py
+++ visnovel/__init__.py
@@ -10,14 +10,19 @@ class VisNovel:
         #  screen_size: tuple[int, int], title: str
         self.config = configs
         pg.init()
-        self.config["default_font"] = self.config[
-            "default_font"] if "default_font" in self.config else "Roboto-Regular.ttf"
+        default_font = f"{__file__.replace('__init__.py', '')}Roboto-Regular.ttf"
+        self.config["default_font"] = (
+            self.config["default_font"]
+            if "default_font" in self.config
+            else default_font
+        )
         self.default_font = pg.font.Font(self.config["default_font"], 32)
         self.screen = pg.display.set_mode(self.config["screen_size"])
         pg.display.set_caption(self.config["title"])
         self.running = True
         self.game_queue = []
         self.current_queue = []
+        self.rendered_queue = []
         self.text_screen(bg=(0, 0, 0), name="")
 
     def mainloop(self) -> None:
@@ -27,44 +32,65 @@ class VisNovel:
                     self.running = False
                     pg.quit()
 
-            for action in self.game_queue:
-                if action["ulid"] not in [current_action["ulid"] for current_action in self.current_queue]:
-                    self.current_queue.append(action)
+            if self.game_queue != self.rendered_queue:
+                for action in self.game_queue:
+                    if action["ulid"] not in [
+                        current_action["ulid"] for current_action in self.current_queue
+                    ]:
+                        self.current_queue.append(action)
 
-            for action in self.current_queue:
-                if action["time_started"] is not None:
-                    if pg.time.get_ticks() - action["time_started"] > action["duration"] * 1000:
-                        action = None
-                self.screen.blit(action["surface"], (0, 0))
-                action["time_started"] = pg.time.get_ticks() if action["time_started"] else action["time_started"]
+                for action in self.current_queue:
+                    if action["time_started"] is not None:
+                        if (
+                            pg.time.get_ticks() - action["time_started"]
+                            > action["duration"] * 1000
+                        ):
+                            action = None
+                    self.screen.blit(action["surface"], (0, 0))
+                    action["time_started"] = (
+                        pg.time.get_ticks()
+                        if action["time_started"]
+                        else action["time_started"]
+                    )
+                    pg.display.flip()
+                self.current_queue = [
+                    action for action in self.current_queue if action is not None
+                ]
+
+                # remove actions that have expired from game_queue
+                self.game_queue = []
                 pg.display.flip()
-            self.current_queue = [action for action in self.current_queue if action is not None]
 
-            # remove actions that have expired from game_queue
-            self.game_queue = []
-            pg.display.flip()
-
-    def text_screen(self,
-                    bg: tuple[int, int, int] = (0, 0, 0),
-                    name: str = "Act 1",
-                    duration: int = 5
-                    ) -> None:
+    def text_screen(
+        self,
+        bg: tuple[int, int, int] = (0, 0, 0),
+        name: str = "Act 1",
+        duration: int = 5,
+    ) -> None:
         # create new surface the size of screen
         screen_copy = self.screen.copy()
         screen_copy.fill(bg)
         text = self.default_font.render(name, True, (255, 255, 255))
         text_rect = text.get_rect()
         # center the text
-        text_rect.center = (self.config["screen_size"][0] // 2, self.config["screen_size"][1] // 2)
+        text_rect.center = (
+            self.config["screen_size"][0] // 2,
+            self.config["screen_size"][1] // 2,
+        )
         # blit the text onto the screen
         screen_copy.blit(text, text_rect)
-        self.game_queue.append({
-            "type": "TextScreen",
-            "ulid": ulid.new(),
-            "surface": screen_copy,
-            "duration": duration,
-            "time_started": None
-        })
+        self.game_queue.append(
+            {
+                "type": "TextScreen",
+                "ulid": ulid.new(),
+                "surface": screen_copy,
+                "duration": duration,
+                "time_started": None,
+            }
+        )
+
+    def character_screen(self):
+        pass
 
 
 if __name__ == "__main__":
@@ -73,5 +99,22 @@ if __name__ == "__main__":
         "title": "Adventure Novel",
     }
     vn = VisNovel(config)
-    vn.text_screen(name="Act 1")
+    alice = Character(
+        name="Alice",
+        states=[
+            {"emotion": "happy", "image_path": "example/alice_happy.png"},
+            {"emotion": "sad", "image_path": "example/alice_sad.png"},
+        ],
+        default="happy",
+    )
+    bob = Character(
+        name="Bob",
+        states=[
+            {"emotion": "happy", "image_path": "example/bob_happy.png"},
+            {"emotion": "sad", "image_path": "example/bob_sad.png"},
+        ],
+        default="happy",
+    )
+    vn.text_screen(name="Act 1", duration=5)
+    vn.scene(bg="example/bg.png", characters=[alice, bob])
     vn.mainloop()
--- visnovel/character.py
+++ visnovel/character.py
@@ -5,20 +5,20 @@ import os
 
 class Character:
     def __init__(
-        self, name: str, emotion_states: List[dict], default_emotion: str
+        self, name: str, states: List[dict], default: str
     ) -> None:
         self.name = name
         self.emotions = {}
-        for emotion in emotion_states:
-            if emotion.emotion in self.emotions:
+        for emotion in states:
+            if emotion["emotion"] in self.emotions:
                 raise KeyError(
                     f"Attempted to define {emotion.emotion} twice for {self.name}."
                 )
             else:
-                self.emotions[emotion.emotion] = emotion.image_path
+                self.emotions[emotion["emotion"]] = emotion["image_path"]
 
                 # Define a new function for this emotion
-                def emotion_func(self, emotion=emotion.emotion):
+                def emotion_func(self, emotion=emotion["emotion"]):
                     # create pygame surface
                     surface = pg.image.load(self.emotions[emotion])
                     # resize the surface
@@ -27,13 +27,13 @@ class Character:
                     self.screen.blit(surface, (0, 0))
 
                 # Assign the function as an attribute of this instance
-                setattr(self, emotion.emotion, emotion_func.__get__(self))
+                setattr(self, emotion["emotion"], emotion_func.__get__(self))
 
         try:
-            self.emotions["default"] = self.emotions[default_emotion]
+            self.emotions["default"] = self.emotions[default]
         except KeyError:
             raise KeyError(
-                f"Attempted to define {self.name}'s default emotion as '{default_emotion}', which does not exist."
+                f"Attempted to define {self.name}'s default emotion as '{default}', which does not exist."
             )
 
     def __str__(self) -> str:
